1f245e428c96e36ba10bf46f65c9a5ea85119acc
Aleksandr <aleksandr.rogachev1994@gmail.com>
2017-06-05 13:07:40 -0400

    scroll messages board automatically

--- src/components/chatrooms/chatroomBoard/ChatroomBoard.js
+++ src/components/chatrooms/chatroomBoard/ChatroomBoard.js
@@ -4,29 +4,41 @@ import { formatDate } from '../../../lib/shared'
 import Message from './Message'
 import MessageForm from './MessageForm'
 
-const ChatroomBoard = (props) => {
+class ChatroomBoard extends React.Component {
 
-  let messages
-  if(props.messages) messages = props.messages.map((msg, i) => (
-    <div key={msg.id} >
-      {i === 0 && <p className="separator">{formatDate(msg.created_at)}</p>}
-      {(i > 0) && (new Date(msg.created_at).toDateString() !== new Date(props.messages[i-1].created_at).toDateString()) && (
-        <p className="separator">{formatDate(msg.created_at)}</p>
-      )}
-      <Message content={msg.content} owner={msg.owner} created_at={msg.created_at} currentUser={props.currentUser} />
-    </div>
-  ))
+  componentDidMount() {
+    let objDiv = document.getElementById("messages");
+    objDiv.scrollTop = objDiv.scrollHeight;
+  }
 
-  return (
-    <div>
-      <div id="messages">
-        {messages}
+  componentDidUpdate() {
+    let objDiv = document.getElementById("messages");
+    objDiv.scrollTop = objDiv.scrollHeight;
+  }
+
+  render() {
+    let messages
+    if(this.props.messages) messages = this.props.messages.map((msg, i) => (
+      <div key={msg.id} >
+        {i === 0 && <p className="separator">{formatDate(msg.created_at)}</p>}
+        {(i > 0) && (new Date(msg.created_at).toDateString() !== new Date(this.props.messages[i-1].created_at).toDateString()) && (
+          <p className="separator">{formatDate(msg.created_at)}</p>
+        )}
+        <Message content={msg.content} owner={msg.owner} created_at={msg.created_at} currentUser={this.props.currentUser} />
       </div>
+    ))
+
+    return (
       <div>
-        <MessageForm chatroom_id={props.chatroom_id} />
+        <div id="messages">
+          {messages}
+        </div>
+        <div>
+          <MessageForm chatroom_id={this.props.chatroom_id} />
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 ChatroomBoard.propTypes = {
--- src/components/chatrooms/chatroomBoard/Message.js
+++ src/components/chatrooms/chatroomBoard/Message.js
@@ -6,44 +6,48 @@ import { formatDateMessage } from '../../../lib/shared'
 const Message = ({ content, owner, created_at, currentUser }) => {
 
   return currentUser.id === owner.id ? (
-    <div className="row">
-      <article className="row col-xs-8 col-xs-offset-4">
-        <div className="message col-xs-10">
-          <div className="message-header">
-            <div style={{textAlign: 'right'}}>
-              <p className="msg-date">{formatDateMessage(created_at)}</p>
+    <div className="container-fluid">
+      <div className="row">
+        <article className="row col-xs-8 col-xs-offset-4">
+          <div className="message col-xs-10">
+            <div className="message-header">
+              <div style={{textAlign: 'right'}}>
+                <p className="msg-date">{formatDateMessage(created_at)}</p>
+              </div>
             </div>
+            <div className="message-body">{content}</div>
           </div>
-          <div className="message-body">{content}</div>
-        </div>
 
-        <div className="message-author col-xs-2">
-          <Link to={`/users/${owner.id}`}>
-            <p className="msg-user"><img src={owner.avatar_thumb_url} className="avatar-thumb" alt="avatar" /></p>
-            <p className="msg-user">{owner.nickname}</p>
-          </Link>
-        </div>
-      </article>
+          <div className="message-author col-xs-2">
+            <Link to={`/users/${owner.id}`}>
+              <p className="msg-user"><img src={owner.avatar_thumb_url} className="avatar-thumb" alt="avatar" /></p>
+              <p className="msg-user">{owner.nickname}</p>
+            </Link>
+          </div>
+        </article>
+      </div>
     </div>
   ) : (
-    <div className="row">
-      <article className="row col-xs-8">
-        <div className="message-author col-xs-2">
-          <Link to={`/users/${owner.id}`}>
-            <p className="msg-user"><img src={owner.avatar_thumb_url} className="avatar-thumb" alt="avatar" /></p>
-            <p className="msg-user">{owner.nickname}</p>
-          </Link>
-        </div>
-
-        <div className="message col-xs-10">
-          <div className="message-header">
-            <div style={{textAlign: 'right'}}>
-              <p className="msg-date">{formatDateMessage(created_at)}</p>
-            </div>
+    <div className="container-fluid">
+      <div className="row">
+        <article className="row col-xs-8">
+          <div className="message-author col-xs-2">
+            <Link to={`/users/${owner.id}`}>
+              <p className="msg-user"><img src={owner.avatar_thumb_url} className="avatar-thumb" alt="avatar" /></p>
+              <p className="msg-user">{owner.nickname}</p>
+            </Link>
           </div>
-          <div className="message-body">{content}</div>
-        </div>
-      </article>
+
+          <div className="message col-xs-10">
+            <div className="message-header">
+              <div style={{textAlign: 'right'}}>
+                <p className="msg-date">{formatDateMessage(created_at)}</p>
+              </div>
+            </div>
+            <div className="message-body">{content}</div>
+          </div>
+        </article>
+      </div>
     </div>
   )
 }
